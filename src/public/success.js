c20a565830314d525a31b7b343e404c85840c642
Mitchel2003 <avilesmaicol.08@gmail.com>
2024-03-15 22:49:31 -0500

    gestionSalud --> test (24/100%)

--- src/public/success.js
+++ src/public/success.js
@@ -2,8 +2,6 @@ import { validateResetPassword } from '../components/firebase/query.js';
 import { customAlert, selectIcon } from '../components/utils/alerts.js';
 
 try {
-    console.log("message receive");
-
     const form=document.getElementById('resetPassword_form');
 
     form.addEventListener('submit', async function (event) {
@@ -13,18 +11,12 @@ try {
         const confirmPassword = document.getElementById('confirmPassword').value;
         await checkoutPassword(password, confirmPassword);
 
-        console.log("first checkpoint");
-
         const obbCode = getCodeObb();
         await validateResetPassword(obbCode, password);
 
-        console.log("second checkpoint");
-
         const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageResetPasswordSuccess();
         customAlert(title, message, selectIcon(typeAlert));
-
-
-        console.log("endpoint");
+        
         // //send to login with sweetAlert message, button to go
         // window.location.href = 'https://mitchel2003.github.io/Gestion_salud/';
     });
