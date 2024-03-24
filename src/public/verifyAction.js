39b9c305a7cbb896ffbabecdea92bc344bb3beeb
Mitchel2003 <avilesmaicol.08@gmail.com>
2024-03-23 21:30:35 -0500

    gestionSalud --> test (24/100%)

--- src/public/verifyAction.js
+++ src/public/verifyAction.js
@@ -1,31 +1,14 @@
-//verifyEmail
 import { alertButtonAction, customAlert, selectIcon } from '../components/utils/alerts.js';
 
 const query = getQueryParams();
 const continueURL = query.continueUrl;
 const pageContext = setContext(continueURL);
-document.q
+document.querySelector('.container p-4').innerHTML(pageContext);
 
-const decodeURL = decodeURIComponent(continueURL);
-const url = new URL(decodeURL);
-const userEmail = url.searchParams.get('email');
-const userAccess = url.searchParams.get('access');
+await setFunctions(continueURL);
 
-if (await (await import('../components/firebase/query.js')).isFoundDocumentReference(userEmail)) {
-    const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageTokenVerifyExpired();
-    const response = await alertButtonAction(title, message, selectIcon(typeAlert));
-    if (response) { (await import('../components/utils/view.js')).goToHome(); }
-    throw new Error('Account verified');
-}
-const request = await (await import('../components/firebase/authentication.js')).appenedDocumentReference(userEmail, userAccess);
-
-const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageUserSubmitted();
-const response = await alertButtonAction(title, message, selectIcon(typeAlert));
-if (response) { (await import('../components/utils/view.js')).goToHome(); }
-await (await import('../components/firebase/query.js')).offSession();
-/*--------------------------------------------------tools--------------------------------------------------*/
-function setContext(data) {//AC #205
-    if (data) {
+function setContext(res) {//AC #205
+    if (res) {
         return `   
         <div class="row">
             <div class="col-md-6 offset-md-3">
@@ -61,52 +44,72 @@ function setContext(data) {//AC #205
         `;
     }
 }
-function getQueryParams() {
-    const queryString = window.location.search;
-    const searchParams = new URLSearchParams(queryString);
-    return Object.fromEntries(searchParams.entries());
-}
+async function setFunctions(res) {
+    if (res) {//verifyEmail
+        const decodeURL = decodeURIComponent(res);
+        const url = new URL(decodeURL);
+        const userEmail = url.searchParams.get('email');
+        const userAccess = url.searchParams.get('access');
 
-//resetPassword
-const form = document.getElementById('resetPassword_form');
-form.addEventListener('submit', async function (event) {//AC #204
-    try {
-        event.preventDefault();
-        const getAlerts = await import('../components/utils/alerts.js');
-
-        const oobCode = getCodeOob();
-        const password = document.getElementById('newPassword').value;
-        const confirmPassword = document.getElementById('confirmPassword').value;
-
-        if (checkSamePasswords(password, confirmPassword)) {
-            const { title, message, typeAlert } = getAlerts.messagePasswordNotSame();
-            customAlert(title, message, selectIcon(typeAlert));
+        if (await (await import('../components/firebase/query.js')).isFoundDocumentReference(userEmail)) {
+            const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageTokenVerifyExpired();
+            const response = await alertButtonAction(title, message, selectIcon(typeAlert));
+            if (response) { (await import('../components/utils/view.js')).goToHome(); }
             return;
         }
-        if (checkSizeAllowed(password)) {
-            const { title, message, typeAlert } = getAlerts.messagePasswordSizeShort();
-            customAlert(title, message, selectIcon(typeAlert));
-            return;
-        }
-        await (await import('../components/firebase/query.js')).validateResetPassword(oobCode, password);
+        await (await import('../components/firebase/authentication.js')).appenedDocumentReference(userEmail, userAccess);
 
-        const { title, message, typeAlert } = getAlerts.messageResetPasswordSuccess();
-        const request = await getAlerts.alertButtonAction(title, message, selectIcon(typeAlert));
-        if (request) { (await import('../components/utils/view.js')).goToHome(); }
+        const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageUserSubmitted();
+        const response = await alertButtonAction(title, message, selectIcon(typeAlert));
+        if (response) { (await import('../components/utils/view.js')).goToHome(); }
         await (await import('../components/firebase/query.js')).offSession();
 
-    } catch (error) {
-        const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageTokenExpired();
-        customAlert(title, message, selectIcon(typeAlert));
-        throw new Error('Token expired, generate a new token');
+    } else {//resetPassword
+        const form = document.getElementById('resetPassword_form');
+        form.addEventListener('submit', async function (event) {//AC #204
+            try {
+                event.preventDefault();
+                const getAlerts = await import('../components/utils/alerts.js');
+
+                const oobCode = getCodeOob();
+                const password = document.getElementById('newPassword').value;
+                const confirmPassword = document.getElementById('confirmPassword').value;
+
+                if (checkSamePasswords(password, confirmPassword)) {
+                    const { title, message, typeAlert } = getAlerts.messagePasswordNotSame();
+                    customAlert(title, message, selectIcon(typeAlert));
+                    return;
+                }
+                if (checkSizeAllowed(password)) {
+                    const { title, message, typeAlert } = getAlerts.messagePasswordSizeShort();
+                    customAlert(title, message, selectIcon(typeAlert));
+                    return;
+                }
+                await (await import('../components/firebase/query.js')).validateResetPassword(oobCode, password);
+
+                const { title, message, typeAlert } = getAlerts.messageResetPasswordSuccess();
+                const request = await getAlerts.alertButtonAction(title, message, selectIcon(typeAlert));
+                if (request) { (await import('../components/utils/view.js')).goToHome(); }
+                await (await import('../components/firebase/query.js')).offSession();
+
+            } catch (error) {
+                const { title, message, typeAlert } = (await import('../components/utils/alerts.js')).messageTokenExpired();
+                customAlert(title, message, selectIcon(typeAlert));
+            }
+        });
     }
-});
+}
 /*--------------------------------------------------tools--------------------------------------------------*/
 function checkSamePasswords(item_1, item_2) {
     if (item_1 !== item_2) { return item_1; }
 }
 function checkSizeAllowed(newPassword) {
     if (newPassword.length <= 6) { return newPassword; }
+}
+function getQueryParams() {
+    const queryString = window.location.search;
+    const searchParams = new URLSearchParams(queryString);
+    return Object.fromEntries(searchParams.entries());
 }
 function getCodeOob() {
     const queryString = window.location.search;
