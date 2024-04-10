2e440efd148203ce35924eb31f62756ce196f16d
Mitchell2003 <avilesmaicol.08@gmail.com>
2024-04-10 17:01:39 -0500

    gestionSalud --> test (26/100%)

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -80,7 +80,7 @@ function applyContext(res) {//AC #205
     } if (res === 'auxiliary') {
         document.title = "Session";
         appenedStyles('../controller/styles/session.css');
-        appenedBackgroundImage("./src/components/images/background_session.webp");
+        appenedBackgroundImage("background_session.webp");
         return `
         <nav>
             <ul class="side-bar">
