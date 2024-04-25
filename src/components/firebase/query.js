849b4e1781fccec7836d2990733679c6c84b7ed0
Mitchel2003 <avilesmaicol.08@gmail.com>
2024-04-24 23:06:14 -0500

    temp (27/100%)

--- src/components/firebase/query.js
+++ src/components/firebase/query.js
@@ -12,9 +12,25 @@ export async function getDocumentUser(user) {
     querySnapshot.forEach((doc) => { const value = doc.data(); access = value.access; key = value.key; });
     return { access, key };
 }
-export async function getDataCollection(object) {//for big data query (listCollections contain min 2 elements)
-    const key = Object.keys(object);
+export async function getDataCollection(request, typeSearch) {//for big data query (listCollections contain min 2 elements)
+    
+    if(typeSearch === 'query'){//query or collection
+
+    }
     if (key.includes('2')) { await checkExtensionData(object); return; }
+
+    if (request === 'user') {
+        
+    }
+    if (request === 'departament') {
+        
+    }
+    if (request === 'device') {
+
+    }
+    if (request === 'finding') {
+        
+    }
 
     //query all entities
     const ask = await getCollection("main");
@@ -39,6 +55,42 @@ export async function isSmallData(){
 export async function getCollection(context) {
     const collectionReference = await collection(db, context);
     return collectionReference;
+}
+export async function pullCollection(object, deep) {
+    const init = doc(db, 'main', object['1']);
+    
+    if (deep === 1) {//user and departament
+        const subCollection = collection(init, object['2']);
+        const querySnapshot = await getDocs(subCollection);
+        return querySnapshot;
+    }
+    if (deep === 2) {//device
+        
+        const querySnapshot = await getDocs(subCollection);
+        return querySnapshot;
+    }
+    if (deep === 3) {//finding
+        
+    }
+    
+}
+export async function pullQuery(object, deep, condicion){//get document apply condicion
+    const init = doc(db, 'main', object['1']);
+    let collectionContext;
+    if (deep === 1) {//user and departament
+        collectionContext = collection(init, object['2']);
+    }
+    if (deep === 2) {//device
+        const subDocument = doc(init, object['2'], object['3']);
+        collectionContext = collection(subDocument, object['4']);
+        
+    }
+    if (deep === 3) {//finding
+        
+    }
+    const ask = query(collection(init, object['2']), where(condicion['p1'], condicion['o1'], condicion['p2']));
+        const querySnapshot = await getDocs(ask);
+        return querySnapshot;
 }
 export function getQueryParams() {
     const queryString = window.location.search;
