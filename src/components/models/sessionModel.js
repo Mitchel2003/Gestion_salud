785f667f01ab9cce926409baa288faaca4b62043
Mitchel2003 <avilesmaicol.08@gmail.com>
2024-04-30 17:51:56 -0500

    apply bootstrap (28/100%)

--- src/components/models/sessionModel.js
+++ src/components/models/sessionModel.js
@@ -1,7 +1,5 @@
 export async function modeAuxiliary() {
-    //side bar
-    document.getElementById('menu-action').addEventListener('click', () => { document.querySelector('.side-bar').classList.add('spawn'); });
-    document.getElementById('close-action').addEventListener('click', () => { document.querySelector('.side-bar').classList.remove('spawn'); });
+    
 }
 export async function modeAuditor() {
 
--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -24,30 +24,21 @@ function getUserContext(res) {//AC #205
     document.title = "Session";
     if (res === 'auxiliary') {
         return `
-        <nav>
-            <ul class="side-bar">
-                <li id="close-action"><a><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
-                <li><a>inicio</a></li>
-                <li><a>Management accounts</a></li>
-                <li><a>Settings</a></li>
-            </ul>
-            <ul>
-                <li><a>Dashboard</a></li>
-                <li class="options"><a>Home</a></li>
-                <li class="options"><a>Manager</a></li>
-                <li class="options"><a>blog</a></li>
-                <li id="menu-action"><a><svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
-            </ul>
-        </nav>
-
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <a class="navbar-brand">Dashboard</a>
+                
+
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="navbar-brand"><img src="../components/images/gs_icon.svg" alt="icon" width="40" height="40"></a>    
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -74,6 +65,9 @@ function getUserContext(res) {//AC #205
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
+
+                    <a class="ms-lg-3 mb-lg-0"> <span class="bx bxs-user-circle"></span> </a>
+
                 </div>
             </div>
         </nav>
