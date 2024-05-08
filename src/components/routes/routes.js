1dbc1023ecd3508ef55fb5d9f8340b60da61208c
mitchel2003 <avilesmaicol.08@gmail.com>
2024-05-08 17:44:13 -0500

    temp (29/100%)

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -106,13 +106,20 @@ function getUserContext(res) {//AC #205
             <div class="tab-panel p-3 fade collapse" id="handler-device" role="tabpanel" aria-labelledby="nav-handler-device">
 
                 <div class="row mb-1">
-                    <nav class="col-lg-8 col-md-8 col-sm-12">
-                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+
+                    <button class="navbar-toggler accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHandlerDevice" aria-controls="navbarHandlerDevice" aria-expanded="false" aria-label="Toggle navigation handler device">
+                        show
+                    </button>
+
+                    <nav class="col-lg-8 col-md-8 col-sm-12 navbar navbar-expand-lg">    
+
+                        <div class="nav navbar-collapse collapse nav-tabs" id="navbarHandlerDevice" role="tablist">
+                            <button class="nav-link active" id="nav-reports" data-bs-toggle="tab" data-bs-target="#reports" type="button" role="tab" aria-controls="reports" aria-selected="true">
+                                Reports
+                            </button>
                             <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
-                            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
-                            <button class="nav-link" id="nav-disabled-tab" data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab" aria-controls="nav-disabled" aria-selected="false" disabled>Disabled</button>
                         </div>
+
                     </nav>
 
                     <form class="d-flex ms-auto col-lg-4 col-md-4 col-sm-12">
@@ -131,8 +138,8 @@ function getUserContext(res) {//AC #205
                             </div>
 
 
-                            <div class="tab-content" id="nav-tabContent">
-                                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">...</div>
+                            <div class="tab-content" id="nav-tabContent-handlerDevice">
+                                <div class="tab-pane fade show active" id="nav-reports" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">...</div>
                                 <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">...</div>
                                 <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">...</div>
                                 <div class="tab-pane fade" id="nav-disabled" role="tabpanel" aria-labelledby="nav-disabled-tab" tabindex="0">...</div>
